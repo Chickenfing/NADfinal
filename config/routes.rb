a1b53a88c7d49e0741a75d5e7ddba2973f814254
Brad Carradine <bcarradine6486@conestogac.on.ca>
2017-12-11 14:00:05 -0500

    Added devise as a gm and installed it.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'hello_page/hello'
-  root 'hello_page#hello'
+  root 'home#index'
+  
+  #get 'hello_page/hello'
+  #root 'hello_page#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
