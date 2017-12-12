9a123123cab16a375fa110bdf1f73116b7d53de9
Brad Carradine <bcarradine6486@conestogac.on.ca>
2017-12-12 01:07:55 -0500

    Now has some of the required controllers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#delete'
+
+  resources :postings
+  resources :courses
   
-  #get 'hello_page/hello'
-  #root 'hello_page#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
