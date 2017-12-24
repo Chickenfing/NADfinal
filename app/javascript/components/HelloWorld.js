57943912f4b51112ed3b7554a88f0acda2c6cbde
Brad Carradine <bcarradine6486@conestogac.on.ca>
2017-12-24 03:18:28 -0500

    Header component started.

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -2,18 +2,33 @@ import React from "react"
 import PropTypes from "prop-types"
 import styled from 'styled-components'
 
+const Span= styled.span`
+    color:red;
+`
+
 const Text = styled.h1`
     font-size: 36px;
     color: grey;
+    ${Span} {
+        color: blue;
+    }
 `;
 
 class HelloWorld extends React.Component {
   render () {
-    return (
-      <div>
-        <Text>Greeting: {this.props.greeting}</Text>
-      </div>
-    );
+      const { greeting }=this.props;
+
+        if (!greeting){
+          return null;
+        }
+        return (
+            <div>
+                <Text>
+                    Greeting: {greeting}
+                </Text>
+                <Span>Hello</Span>
+            </div>
+        );
   }
 }
 
---
+++ app/javascript/components/header/index.js
@@ -0,0 +1,34 @@
+import React { Component } from 'react'
+import styled from 'styled-components'
+
+const HeaderLogo = styled.img `
+
+
+`;
+
+
+const HeaderDiv = styled.div `
+
+
+
+`;
+
+
+const HeaderIcons = styled.div `
+
+
+`;
+
+const HeaderSearch = stylef.text `
+
+
+`;
+
+class Header extends Component {
+    render() {
+
+    }
+
+}
+
+export default Header;
